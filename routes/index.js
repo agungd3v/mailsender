50673b195f124930b5f3b1bd8f2d9868c1e71c3a
agungd3v <mailtotuyul1@gmail.com>
2021-09-16 21:56:07 +0700

    update route mail to /

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 
 const MailController = require("../controllers/MailController")
 
-router.get('/mail', MailController.index)
+router.get('/', MailController.index)
 router.post('/read', MailController.read)
 router.post('/send', MailController.send)
 
